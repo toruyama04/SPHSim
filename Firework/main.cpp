63994316f9d1b6a6a4caf710da66209c8321bdf4
Toru Yamaguchi <txy212@student.bham.ac.uk>
2024-06-06 00:11:04 +0100

    basic firework implementation with instancing

--- Firework/main.cpp
+++ Firework/main.cpp
@@ -4,20 +4,32 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/random.hpp>
 
 #include "shader.h"
 #include "camera.h"
 
+#include <vector>
+
+struct Particle {
+    glm::vec3 position;
+    glm::vec3 velocity;
+    float lifetime;
+};
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void processInput(GLFWwindow* window);
+void updateParticles(std::vector<Particle>& particles, float deltaTime);
+void createFirework(std::vector<Particle>& particles, const glm::vec3& position, int count);
+void updateVertexData(const std::vector<Particle>& particles, std::vector<float>& vertexData);
 
 // settings
 const unsigned int SCR_WIDTH = 3000;
 const unsigned int SCR_HEIGHT = 2500;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 25.0f));
 float lastX = (float)SCR_WIDTH / 2.0;
 float lastY = (float)SCR_HEIGHT / 2.0;
 bool firstMouse = true;
@@ -25,6 +37,8 @@ bool firstMouse = true;
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
+
+int particleNum = 500;
 
 
 int main() {
@@ -53,9 +67,57 @@ int main() {
     }
 
     // global states
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_PROGRAM_POINT_SIZE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    float vertices[] = {
+        -0.1f, -0.1f, 0.0f,
+         0.1f, -0.1f, 0.0f,
+         0.1f,  0.1f, 0.0f,
+        -0.1f,  0.1f, 0.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    std::vector<Particle> particles;
+
+    createFirework(particles, glm::vec3(0.0f, 15.0f, 0.0f), particleNum);
+
+    GLuint VAO, VBO, EBO, instanceVBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glGenBuffers(1, &instanceVBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, particles.size() * sizeof(glm::vec3), nullptr, GL_DYNAMIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribDivisor(1, 1);
+
+    glBindVertexArray(0);
 
     // shaders
+    Shader particleShader("particle.vert", "particle.frag");
+    particleShader.use();
+    glm::vec3 color = glm::vec3(1.0f);
+    particleShader.setVec3("vColor", color);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -65,13 +127,36 @@ int main() {
 
         processInput(window);
 
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        updateParticles(particles, deltaTime);
+
+        std::vector<glm::vec3> positions(particles.size());
+        for (size_t i = 0; i < particles.size(); ++i) {
+            positions[i] = particles[i].position;
+        }
+        glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, positions.size() * sizeof(glm::vec3), positions.data());
+
+        glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        particleShader.use();
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        particleShader.setMat4("view", view);
+        particleShader.setMat4("projection", projection);
+
+        glBindVertexArray(VAO);
+        glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0, particles.size());
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &instanceVBO);
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
@@ -120,4 +205,26 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
     lastY = ypos;
 
     camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// function to update particles
+void updateParticles(std::vector<Particle>& particles, float deltaTime) {
+    for (auto& particle : particles) {
+        particle.velocity += glm::vec3(0.0f, -1.8f, 0.0f) * deltaTime; // gravity
+        particle.position += particle.velocity * deltaTime;
+        particle.lifetime -= deltaTime;
+    }
+    particles.erase(std::remove_if(particles.begin(), particles.end(),
+        [](const Particle& p) { return p.lifetime <= 0.0f; }), particles.end());
+}
+
+
+void createFirework(std::vector<Particle>& particles, const glm::vec3& position, int count) {
+    for (int i = 0; i < count; ++i) {
+        Particle p;
+        p.position = position;
+        p.velocity = glm::sphericalRand(1.0f); 
+        p.lifetime = 5.0f; 
+        particles.push_back(p);
+    }
 }
